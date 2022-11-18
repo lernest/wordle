2f1479a936439fd5312c33ddb0edfe7436bf75af
lernest <19194046+lernest@users.noreply.github.com>
2022-11-17 20:41:23 -0800

    guessedLetters to set and number of rows

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
 <div>
-    <LetterRow v-for="guess in guesses" :word="guess"/>
-  <LetterRow v-if="!isGameOver" :word="currentGuess"/>
-  <LetterRow v-if="!isGameOver" v-for="index in emptyRows" :key="index" word=""/>
+  <LetterRow v-for="guess in guesses" :word="guess"/>
+  <LetterRow v-if="guesses.length<6" :word="currentGuess"/>
+  <LetterRow v-for="index in emptyRows" :key="index" word="empty"/>
 </div>
 <Keyboard v-on:keyPressed="addLetter"/>
 </template>
@@ -18,19 +18,19 @@ export default {
   },
   computed:{
     emptyRows(){
-      return 4-this.guesses.length >= 0 ? 4-this.guesses.length : 0
+      return 5-this.guesses.length >= 0 ? 5-this.guesses.length : 0
     },
     isGameOver(){
-      return this.guesses.length === 5
+      return this.guesses.length === 6 || this.guessedLetters.green.size === 5
     }
   },
   data(){
     return{
       target: 'maple',
       guessedLetters: {
-        gray: [],
-        yellow: [],
-        green: []
+        gray: new Set(),
+        yellow: new Set(),
+        green: new Set()
       },
       guesses: [],//['raise','earth','maple'],
       currentGuess:'',
@@ -41,15 +41,15 @@ export default {
       if(!this.isGameOver){
 
         if(letter === 'backspace' && this.currentGuess.length > 0){
-          this.currentGuess = this.currentGuess.substring(0,this.currentGuess.length-1)
-      }
-      else if(letter !== 'enter' && letter !== 'backspace' && this.currentGuess.length < 5){
-        this.currentGuess+=letter
-      }
-      else if(letter === 'enter' && this.currentGuess.length == 5){
-        this.submitGuess()
-      }
+            this.currentGuess = this.currentGuess.substring(0,this.currentGuess.length-1)
         }
+        else if(letter !== 'enter' && letter !== 'backspace' && this.currentGuess.length < 5){
+          this.currentGuess+=letter
+        }
+        else if(letter === 'enter' && this.currentGuess.length == 5){
+          this.submitGuess()
+        }
+      }
       console.log(this.currentGuess)
     },
     submitGuess(){
@@ -61,30 +61,53 @@ export default {
     evaluateGuess(guess){
       let guessArray = guess.split('')
       let targetArray = this.target.split('')
+
+      let removed = []
       
       // check for exact matches
       guessArray.forEach((c,i)=>{
+        console.log(`--check for exact matches ${c} == ${targetArray[i]}`)
         if(targetArray[i]===c){
-          this.guessedLetters.green.push(c)
-          // remove the guessed letter from the target array
-          targetArray.splice(i,1)
-          guessArray.splice(i,1)
+          console.log(`adding ${c} to green`)
+          this.guessedLetters.green.add(c)
+          // "remove" the guessed letter from the target array
+          removed.push(c)
+
+          console.log(`         targetArray: ${targetArray}`)
+          console.log(`         guessArray: ${guessArray}`)
+          console.log(`         greenArray: ${new Array(...this.guessedLetters.green).join(' ')}`)
         }
       })
 
       // check for yellows
+    guessArray = guessArray.filter(x => !removed.includes(x))
+    targetArray = targetArray.filter(x => !removed.includes(x))
+
       for(let i=0; i<guessArray.length; i++){
         for(let j=0; j<targetArray.length; j++){
           if(guessArray[i] === targetArray[j]){
-            this.guessedLetters.yellow.push(guessArray[i])
-            targetArray.splice(i,1)
-            guessArray.splice(i,1)
+            console.log(`adding ${guessArray[i]} to yellow`)
+            this.guessedLetters.yellow.add(guessArray[i])
+            removed.push(guessArray[i])
+            console.log(`         targetArray: ${targetArray}`)
+            console.log(`         guessArray: ${guessArray}`)
+            console.log(`         yellowArray: ${new Array(...this.guessedLetters.yellow).join(' ')}`)
           }
         }
       }
 
       // the rest are gray
-      guessArray.forEach(letter => this.guessedLetters.gray.push(letter))
+
+      guessArray = guessArray.filter(x => !removed.includes(x))
+      targetArray = targetArray.filter(x => !removed.includes(x))
+
+      guessArray.forEach(letter => {
+        console.log(`adding ${letter} to gray`)
+        this.guessedLetters.gray.add(letter)
+        console.log(`         targetArray: ${targetArray}`)
+        console.log(`         guessArray: ${guessArray}`)
+        console.log(`         grayArray: ${new Array(...this.guessedLetters.gray).join(' ')}`)
+        })
 
       console.log(this.guessedLetters)
 
